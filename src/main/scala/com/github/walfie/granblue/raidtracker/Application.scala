ed57f53aa9ccfdcbb808004457c5b034d2008211
Walfie <walfington@gmail.com>
2016-08-07 20:58:27 -0400

    Move class into WebsocketFlow object

--- src/main/scala/com/github/walfie/granblue/raidtracker/Application.scala
+++ src/main/scala/com/github/walfie/granblue/raidtracker/Application.scala
@@ -4,14 +4,9 @@ import akka.actor._
 import akka.cluster.pubsub.DistributedPubSub
 import akka.cluster.pubsub.DistributedPubSubMediator.{Subscribe, SubscribeAck}
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model._
-import akka.http.scaladsl.model.ws._
 import akka.http.scaladsl.server.Directives._
-import akka.NotUsed
-import akka.stream._
-import akka.stream.scaladsl._
+import akka.stream.ActorMaterializer
 import com.github.walfie.granblue.raidtracker.actor._
-import java.util.UUID
 import scala.io.StdIn
 
 // TODO: Make this not so bad
@@ -21,38 +16,9 @@ object Application {
   def createRoutes(
     system: ActorSystem, raidPoller: ActorRef, pubSubMediator: ActorRef
   ) = path("ws") {
-    handleWebSocketMessages(newSubscriber(system, raidPoller, pubSubMediator))
+    handleWebSocketMessages(WebsocketFlow.newSubscriber(system, raidPoller, pubSubMediator))
   } ~ pathPrefix("") {
     encodeResponse(getFromResourceDirectory("static"))
-  }
-
-  def newSubscriber(
-    system: ActorSystem, raidPoller: ActorRef, pubSubMediator: ActorRef
-  ): Flow[Message, Message, NotUsed] = {
-    val props = Props(
-      new WebsocketHandler(raidPoller, pubSubMediator)
-    )
-
-    val subscriber = system.actorOf(props, "raid-subscriber-" + UUID.randomUUID())
-
-    val incomingMessages: Sink[Message, NotUsed] = Flow[Message].map {
-      case TextMessage.Strict(text) => // parse json
-        subscriber ! WebsocketHandler.Protocol.RaidBossesRequest
-      case _ => // Ignore
-    }.to(Sink.actorRef[Any](subscriber, PoisonPill))
-
-    val outgoingMessages: Source[Message, NotUsed] = {
-      val bufferSize = 100
-      Source.actorRef[Any](bufferSize, OverflowStrategy.fail)
-        .mapMaterializedValue { out =>
-          subscriber ! WebsocketHandler.WebsocketConnected(out)
-          NotUsed
-        }.map { msg: Any =>
-          TextMessage(msg.toString) // TODO: Make this not bad
-        }
-    }
-
-    Flow.fromSinkAndSource(incomingMessages, outgoingMessages)
   }
 
   def main(args: Array[String]): Unit = {
@@ -73,36 +39,6 @@ object Application {
     scala.io.StdIn.readLine()
     println("Stopping server.")
     system.terminate()
-  }
-
-  // For temporary debugging
-  class SubscriberActor(
-    raidPoller: ActorRef, mediator: ActorRef
-  ) extends Actor {
-    import RaidPoller._
-
-    raidPoller ! GetRaidBosses
-
-    /*
-    val following = Set("Lv60 ユグドラシル・マグナ")
-    following.foreach { raidBoss =>
-      // TODO: Use second param for optional group?
-      // Subscribe(topic: String, group: Option[String], ref: ActorRef)
-      mediator ! Subscribe(raidBoss, self)
-      raidPoller ! GetCachedRaids(raidBoss)
-    }
-    */
-
-    def receive: Receive = {
-      case RaidBossesMessage(raidBosses) =>
-        println("Known raid bosses:")
-        raidBosses.foreach(println)
-        raidBosses.foreach(raidBoss => mediator ! Subscribe(raidBoss.name, self))
-
-      case RaidsMessage(bossName, raids) =>
-        println("Received raids:")
-        raids.foreach(println)
-    }
   }
 }
 
---
+++ src/main/scala/com/github/walfie/granblue/raidtracker/actor/WebsocketFlow.scala
@@ -0,0 +1,41 @@
+package com.github.walfie.granblue.raidtracker.actor
+
+import akka.actor._
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.ws._
+import akka.NotUsed
+import akka.stream._
+import akka.stream.scaladsl._
+import java.util.UUID
+
+object WebsocketFlow {
+  def newSubscriber(
+    system: ActorSystem, raidPoller: ActorRef, pubSubMediator: ActorRef
+  ): Flow[Message, Message, NotUsed] = {
+    val props = Props(
+      new WebsocketHandler(raidPoller, pubSubMediator)
+    )
+
+    val subscriber = system.actorOf(props, "raid-subscriber-" + UUID.randomUUID())
+
+    val incomingMessages: Sink[Message, NotUsed] = Flow[Message].map {
+      case TextMessage.Strict(text) => // TODO: parse json
+        subscriber ! WebsocketHandler.Protocol.RaidBossesRequest
+      case _ => // Ignore
+    }.to(Sink.actorRef[Any](subscriber, PoisonPill))
+
+    val outgoingMessages: Source[Message, NotUsed] = {
+      val bufferSize = 100
+      Source.actorRef[Any](bufferSize, OverflowStrategy.fail)
+        .mapMaterializedValue { out =>
+          subscriber ! WebsocketHandler.WebsocketConnected(out)
+          NotUsed
+        }.map { msg: Any =>
+          TextMessage(msg.toString) // TODO: Make this not bad
+        }
+    }
+
+    Flow.fromSinkAndSource(incomingMessages, outgoingMessages)
+  }
+}
+
