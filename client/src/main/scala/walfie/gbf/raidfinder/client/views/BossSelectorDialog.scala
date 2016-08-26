ce8283b3323a02a88ccf799af4ee09df5574d257
Walfie <walfington@gmail.com>
2016-08-26 00:05:24 -0400

    Add dialog element (modal)

---
+++ client/src/main/scala/walfie/gbf/raidfinder/client/views/BossSelectorDialog.scala
@@ -0,0 +1,64 @@
+package walfie.gbf.raidfinder.client.views
+
+import com.thoughtworks.binding
+import com.thoughtworks.binding.Binding
+import com.thoughtworks.binding.Binding._
+import org.scalajs.dom
+import org.scalajs.dom.raw._
+import scala.scalajs.js
+import walfie.gbf.raidfinder.client.RaidBossColumn // TODO: Move this
+import walfie.gbf.raidfinder.protocol._
+import walfie.gbf.raidfinder.protocol.RaidBossesResponse.RaidBoss
+
+object BossSelectorDialog {
+  @binding.dom
+  def dialogElement(): Binding[Element] = {
+    val elem = dom.document.createElement("dialog")
+    elem.classList.add("mdl-dialog")
+    elem.classList.add("gbfrf-follow__dialog")
+    val closeModal = { (e: Event) => elem.asInstanceOf[js.Dynamic].close(); () }
+
+    val inner =
+      <div class="gbfrf-follow mdl-layout mdl-layout--fixed-header">
+        { dialogHeader(onClose = closeModal).bind }
+        <div class="gbfrf-follow__content">
+          <ul class="mdl-list" style="padding: 0; margin: 0;">
+            { bossListItem.bind }
+          </ul>
+        </div>
+        <hr style="margin: 0;"/>
+        { dialogFooter(onCancel = closeModal).bind }
+      </div>
+
+    elem.appendChild(inner)
+    elem
+  }
+
+  @binding.dom
+  def dialogHeader(onClose: Event => Unit): Binding[HTMLElement] = {
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row gbfrf-column__header-row">
+        <div class="mdl-layout-title gbfrf-column__header">Follow</div>
+        <div class="mdl-layout-spacer"></div>
+        <div class="mdl-button mdl-js-button mdl-button--icon material-icons" onclick={ onClose }>
+          <i class="material-icons">clear</i>
+        </div>
+      </div>
+    </header>
+  }
+
+  @binding.dom
+  def dialogFooter(onCancel: Event => Unit): Binding[HTMLDivElement] = {
+    <div class="mdl-dialog__actions">
+      <button type="button" class="mdl-button" onclick={ onCancel }>Cancel</button>
+    </div>
+  }
+
+  @binding.dom
+  def bossListItem(): Binding[HTMLLIElement] = {
+    <li class="gbfrf-follow__boss_box mdl-list__item mdl-shadow--2dp">
+      <span class="gbfrf-follow__boss_text mdl-list__item-primary-content">Lvl60 Yggdrasil Omega</span>
+    </li>
+  }
+}
+
--- client/src/main/scala/walfie/gbf/raidfinder/client/views/MainContent.scala
+++ client/src/main/scala/walfie/gbf/raidfinder/client/views/MainContent.scala
@@ -4,6 +4,7 @@ import com.thoughtworks.binding
 import com.thoughtworks.binding.Binding
 import com.thoughtworks.binding.Binding._
 import org.scalajs.dom.raw._
+import scala.scalajs.js
 import walfie.gbf.raidfinder.client.RaidBossColumn // TODO: Move this
 import walfie.gbf.raidfinder.protocol._
 import walfie.gbf.raidfinder.protocol.RaidBossesResponse.RaidBoss
@@ -11,8 +12,11 @@ import walfie.gbf.raidfinder.protocol.RaidBossesResponse.RaidBoss
 object MainContent {
   @binding.dom
   def mainContent(bossColumns: BindingSeq[RaidBossColumn]): Binding[HTMLDivElement] = {
+    val dialog = BossSelectorDialog.dialogElement.bind
+
     <div class="gbfrf-main-content">
-      { floatingActionButton.bind }
+      { dialog }
+      { floatingActionButton(dialog).bind }
       <div class="gbfrf-columns">
         {
           bossColumns.map { bossColumn =>
@@ -27,9 +31,9 @@ object MainContent {
   }
 
   @binding.dom
-  def floatingActionButton: Binding[HTMLDivElement] = {
-    // TODO: onclick
-    <div class="gbfrf-settings-fab__container">
+  def floatingActionButton(dialog: Element): Binding[HTMLDivElement] = {
+    val showModal = (e: Event) => dialog.asInstanceOf[js.Dynamic].showModal()
+    <div class="gbfrf-settings-fab__container" onclick={ showModal }>
       <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--primary">
         <i class="material-icons">add</i>
       </button>
