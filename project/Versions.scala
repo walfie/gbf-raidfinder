3800ecdbe75feb5c558d3d7488e9a8477ddf78f3
walfie <walfington@gmail.com>
2016-08-27 13:27:57 -0400

    Update dependency versions (#12)
    Monix 2.0-RC13 fixes some significant bugs

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,9 +1,9 @@
 object Versions {
-  val Akka = "2.4.9-RC2"
+  val Akka = "2.4.9"
   val Mockito = "1.10.19"
-  val Monix = "2.0-RC11"
-  val Play = "2.5.4"
-  val ScalaPB = "0.5.38"
+  val Monix = "2.0-RC13"
+  val Play = "2.5.6"
+  val ScalaPB = "0.5.39"
   val ScalaPB_json4s = "0.1.1"
   val ScalaTest = "3.0.0"
   val Twitter4j = "4.0.4"
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,5 @@
+// sbt-scalapb must be before sbt-scalajs
+// https://github.com/trueaccord/ScalaPB/issues/150#issuecomment-236232402
 addSbtPlugin("com.trueaccord.scalapb" % "sbt-scalapb" % "0.5.38")
 
 addSbtPlugin("org.scalariform" % "sbt-scalariform" % "1.6.0")
