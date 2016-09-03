fc5e01fa96e36f7b70a63ffc1ad075bc7a718a70
walfie <walfington@gmail.com>
2016-09-02 23:06:56 -0400

    Update monix to 2.0.0 (#26)

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
   val Akka = "2.4.9"
   val Mockito = "1.10.19"
-  val Monix = "2.0-RC13"
+  val Monix = "2.0.0"
   val Play = "2.5.6"
   val ScalaPB = "0.5.39"
   val ScalaPB_json4s = "0.1.1"
