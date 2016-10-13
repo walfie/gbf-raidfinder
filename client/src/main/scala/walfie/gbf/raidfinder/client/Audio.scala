8b607a0606e560a06f821f8578023587796c371d
Walfie <walfington@gmail.com>
2016-10-12 20:46:39 -0400

    Import audio files

---
+++ client/src/main/scala/walfie/gbf/raidfinder/client/Audio.scala
@@ -0,0 +1,35 @@
+package walfie.gbf.raidfinder.client
+
+import org.scalajs.dom.raw.HTMLAudioElement
+import scala.scalajs.js
+
+package audio {
+  case class NotificationSound(id: NotificationSoundId, fileName: String) {
+    def play(): Unit = {
+      js.Dynamic.newInstance(js.Dynamic.global.Audio)(pathPrefix + fileName)
+        .asInstanceOf[HTMLAudioElement]
+        .play()
+    }
+  }
+}
+
+package object audio {
+  type NotificationSoundId = Int
+
+  private[audio] val pathPrefix = "audio/"
+
+  val NotificationSounds: Map[NotificationSoundId, NotificationSound] = Seq(
+    0 -> "oh-finally.ogg",
+    1 -> "oringz-w425.ogg",
+    2 -> "pedantic.ogg",
+    3 -> "suppressed.ogg",
+    4 -> "system.ogg",
+    5 -> "tweet.ogg",
+    6 -> "youve-been-informed.ogg",
+    7 -> "you-wouldnt-believe.ogg"
+  // When adding new files, add to bottom of list so IDs are preserved
+  ).map {
+      case (id, fileName) => id -> NotificationSound(id, fileName)
+    }.toMap
+}
+
