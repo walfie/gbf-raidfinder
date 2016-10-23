b21875fe64cadfa567bd6ddb414c267b964d1e12
walfie <walfington@gmail.com>
2016-10-23 19:59:20 -0400

    Update Akka, Monix, Play to latest versions (#86)

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,8 +1,8 @@
 object Versions {
-  val Akka = "2.4.10"
+  val Akka = "2.4.11"
   val Mockito = "1.10.19"
-  val Monix = "2.0.0"
-  val Play = "2.5.6"
+  val Monix = "2.0.4"
+  val Play = "2.5.9"
   val ScalaPB = "0.5.39"
   val ScalaPB_json4s = "0.1.1"
   val ScalaTest = "3.0.0"
